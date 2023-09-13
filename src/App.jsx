07d5429a94c826421b97c84b0e4a535687aabbe8
Michael Santamaria <38706038+michael-146514@users.noreply.github.com>
2023-09-13 14:18:08 -0500

    Axios Get Movies

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,40 @@ import MovieList from "./components/MovieList/MovieList";
 import MovieInfo from "./components/MovieInfo/MovieInfo";
 import NewMovieForm from "./components/NewMovieForm/NewMovieForm";
 import React, { useState, useEffect } from "react";
-import initData from "./data/data";
+import axios from "axios";
 
 function App() {
   const [movies, setMovies] = useState([]);
 
+  const [activeIndex, setActiveIndex] = useState(-1);
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get("https://localhost:7276/api/movies");
+      //console.log(response);
+      setMovies(response.data);
+    } catch (error) {
+      console.warn("Error in fetchMovies request: ", error);
+    }
+  };
+
   useEffect(() => {
-    setMovies(initData);
+    fetchMovies();
   }, []);
 
   const handleNewMovie = (newMovie) => {
     const updatedMovies = [...movies, newMovie];
     setMovies(updatedMovies);
   };
-
-  const selectedMovie = {
-    title: "Forrest Gump",
-    runningTime: 142,
-    genre: "Romantic Drama",
-  };
-
+  const selectedMovie = movies[activeIndex];
   return (
     <div className="App">
       <Header />
       <div className="flex-container">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+        />
         <MovieInfo movieObj={selectedMovie} />
         <NewMovieForm onNewMovie={handleNewMovie} />
       </div>
